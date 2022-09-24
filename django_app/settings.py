6003a8a77f74b6ff23a42692439dfc9bdb8c3d75
Joraev Azam <joraev.azam1252@gmail.com>
2022-09-24 17:27:49 +0500

    done

--- django_app/settings.py
+++ django_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,7 +45,6 @@ INSTALLED_APPS = [
     'apps.teachers.apps.TeachersConfig',
     'apps.partners.apps.PartnersConfig',
     'apps.contact.apps.ContactConfig',
-    'whitenoise.runserver_nostatic',
 
 
 
@@ -58,13 +58,16 @@ REST_FRAMEWORK = {
 }
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'django_app.urls'
@@ -103,13 +106,6 @@ DATABASES = {
     }
 }
 
-# DATABASE_URL=postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}
-# # PGDATABASE=railway
-# # PGHOST=containers-us-west-74.railway.app
-# # PGPASSWORD=9HAj9KdFrWbiHyzdevI6
-# # PGPORT=6798
-# # PGUSER=postgres
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -146,13 +142,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static_cdn'),
-    os.path.join(BASE_DIR, 'media_cdn'),
+    os.path.join(BASE_DIR / 'static'),
+    os.path.join(BASE_DIR / 'media')
 ]
+
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 
 
 # Default primary key field type
--- django_app/wsgi.py
+++ django_app/wsgi.py
@@ -15,4 +15,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
 
 application = get_wsgi_application()
+
 application = WhiteNoise(application, root="static")
